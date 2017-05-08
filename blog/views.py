13ed77f64136cea97132a2d6dff22f6fc00989fd
yjfu <15253112679@163.com>
2017-05-08 09:52:04 +0800

    筛选
    筛选

--- blog/views.py
+++ blog/views.py
@@ -3,129 +3,167 @@ from __future__ import unicode_literals
 
 # Create your views here.
 from django.shortcuts import render
-from django.db import models
+from django.db import models, connection, transaction
 from comment import forms
-from blog.models import Category, Article
+from blog.models import Category, Article, Tag
 from comment.views import getCommentList
 import codecs
 import markdown
 
 
 def findByTag(request):
-    tags = request.GET
-    return render(request, 'linkt.html', context={'content': str(tags)})
+    path = request.GET
+    a = 0
+    tags = list(path.keys())
+
+    cursor = connection.cursor()
+    sql = 'select article_id from blog_article_tags where '
+
+    for tag in tags:
+        a += 1
+        if tag == tags[-1]:
+            sql += ' tag_id = '
+            sql += tag
+        else:
+            sql += ' tag_id = '
+            sql += tag[0]
+            sql += ' OR '
+    sql += ' group by article_id having count(article_id)='
+    sql += str(tags.__len__())
+    sql += ';'
+
+    if a == 0:
+        return index(request)
+    categoryList = getCategoryList()
+    tagList = getTagList()
+
+    cursor.execute(sql)
+    articleIdList = cursor.fetchall()
+
+
+    articleList = []
+    for article_id in articleIdList:
+        tmp = article_id[0]
+        articleList.append(Article.objects.get(id=tmp))
+
+    for article in articleList:
+        article.link = '/article/' + str(article.pk)
+        s = markdown.markdown(article.body, extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        article.body = s
+
+    return render(request, 'index.html', context={'title': "X's Blog",
+                                                  'category_list': categoryList,
+                                                  'tag_list': tagList,
+                                                  'article_list': articleList})
 
 
 def findByKeyWord(request):
-    keyWord = request.GET
-    return render(request, 'linkt.html', context={'content': str(keyWord)})
+    keyWord = request.GET['keyWord']
+    if keyWord == "":
+        return index(request)
+    categoryList = getCategoryList()
+    tagList = getTagList()
+    articleList = []
+    ids = {}
+
+    try:
+        articleList_body = Article.objects.filter(body__contains=keyWord)
+    except Article.DoesNotExist:
+        try:
+            articleList_title = Article.objects.filter(title__contains=keyWord)
+        except Article.DoesNotExist:
+            pass
+    try:
+        articleList_title = Article.objects.filter(title__contains=keyWord)
+    except Article.DoesNotExist:
+        pass
+
+    for article in articleList_body:
+        article.link = '/article/' + str(article.pk)
+        articleList.append(article)
+        ids[article.pk] = 1
+    for article in articleList_title:
+        if ids[article.pk] != 1:
+            article.link = '/article/' + str(article.pk)
+            articleList.append(article)
+
+    return render(request, 'index.html', context={'title': "X's Blog",
+                                                  'category_list': categoryList,
+                                                  'tag_list': tagList,
+                                                  'article_list': articleList})
 
 
 def articlePage(request):
     path = request.path
     articleId = int(path.split(r'/')[-1]) - 1
-    article = [getTestarticleList()[articleId]]
+    article = [getArticleList()[articleId]]
+    catagoryList = getCategoryList()
+    tagList = getTagList()
 
-    catagoryList = getTestCategoryList()
-    tagList = getTestTagList()
+    s = markdown.markdown(article[0].body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    article[0].body = s
 
     return render(request, 'article-body.html', context={'title': "X's Blog",
                                                          'category_list': catagoryList,
                                                          'tag_list': tagList,
                                                          'article_list': article,
                                                          'form': forms.CommentForm(),
-                                                         'comment_list': getCommentList(article_pk=(articleId+1))
+                                                         'comment_list': getCommentList(article_pk=(articleId + 1))
                                                          })
 
 
 def findByCategory(request):
     path = request.path
-    category = path.split(r'/')[-1]
+    category_pk = Category.objects.get(name=path.split(r'/')[-1]).pk
+    catagoryList = getCategoryList()
+    tagList = getTagList()
 
-    return render(request, 'linkt.html', context={'content': category})
+    articleList = Article.objects.filter(category__pk__exact=category_pk)
+
+    for article in articleList:
+        article.link = '/article/' + str(article.pk)
+
+    return render(request, 'index.html', context={'title': "X's Blog",
+                                                  'category_list': catagoryList,
+                                                  'tag_list': tagList,
+                                                  'article_list': articleList})
+
+    # return render(request, 'linkt.html', context={'content': category})
 
 
 def index(request):
-    catagoryList = getTestCategoryList()
-    tagList = getTestTagList()
-    articleList = getTestarticleList()
+    catagoryList = getCategoryList()
+    tagList = getTagList()
+    articleList = getArticleList()
     return render(request, 'index.html', context={'title': "X's Blog",
                                                   'category_list': catagoryList,
                                                   'tag_list': tagList,
                                                   'article_list': articleList})
 
 
-def getTestCategoryList():
-    C = type(str('C'), (), {})
-    category1 = C()
-    category1.name = '类别1'
-    category1.link = '/category/' + category1.name
-    category2 = C()
-    category2.name = '类别2'
-    category2.link = '/category/' + category2.name
-
-    category_list = [category1, category2]
+def getCategoryList():
+    category_list = Category.objects.all()
+    for category in category_list:
+        category.link = '/category/' + category.name
     return category_list
 
 
-def getTestTagList():
-    T = type(str('T'), (), {})
-    tag1 = T()
-    tag1.id = 't1'
-    tag1.name = '标签1'
-    tag2 = T()
-    tag2.id = 't2'
-    tag2.name = '标签2'
-
-    tag_list = [tag1, tag2]
-    return tag_list
+def getTagList():
+    return Tag.objects.all()
 
 
-def getTestarticleList():
-    # A = type(str('A'), (), {})
-    # article1 = A()
-    # article1.articleId = 1
-    # article1.title = '第一篇文章的标题'
-    # article1.author = 'author'
-    # article1.category = 'class1'
-    # article1.create_date = '2017年 四月25日'
-    # article1.excerpt = '这是一篇示例文章,这里是他的简介,' \
-    #                    '可以再长一点再长一点再长一点可以再长一点再' \
-    #                    '长一点再长一点可以再长一点再长一点再长一点' \
-    #                    '可以再长一点再长一点再长一点可以再长一点再' \
-    #                    '长一点再长一点长一点再长一点长一点再长一点长一点' \
-    #                    '再长一点长一点再长一点长一点再长一点长一点再长一点' \
-    #                    '长一点再长一点长一点再长一点长一点再长一点长一点再' \
-    #                    '长一点长一点再长一点长一点再长一点长一点再长一点'
-    # article1.link = '/article/' + str(article1.articleId)
-    # file = codecs.open('blog/test.md', 'r', encoding='utf-8')
-    # s = file.read()
-    #
-    # s = markdown.markdown(s, extensions=[
-    #     'markdown.extensions.extra',
-    #     'markdown.extensions.codehilite',
-    #     'markdown.extensions.toc',
-    # ])
-    # article1.body = s
-    # article2 = A()
-    # article2.articleId = 2
-    # article2.title = '第二篇文章的标题'
-    # article2.author = 'author'
-    # article2.category = 'class1'
-    # article2.create_date = '2017年 四月25日'
-    # article2.excerpt = '这是一篇示例文章,这里是他的简介'
-    # article2.link = '/article/' + str(article2.articleId)
-    # article_list = [article1, article2]
-    # article2.body = '内容'
+def getArticleList():
     articleList = Article.objects.all()
     for article in articleList:
         article.link = '/article/' + str(article.pk)
-    article.body = markdown.markdown(article.body, extensions=[
-        'markdown.extensions.extra',
-        'markdown.extensions.codehilite',
-        'markdown.extensions.toc',
-    ])
     return articleList
 
-# def addCategory(categoryName):
-#
+
--- blogpro/settings.py
+++ blogpro/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'TEST_CHARSET': 'utf8'
-     }
+    }
 }
 '''''''''
 DATABASES = {
